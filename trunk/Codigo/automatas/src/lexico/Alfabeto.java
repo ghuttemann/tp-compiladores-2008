0d6f79e5667840884b831464d22b55218cdeef70
ghuttemann <ghuttemann@03c27c54-a44f-11dd-98aa-2b35b7097d33>
2008-10-30 20:24:12 +0000

    Se agrega el proyecto Netbeans para la parte del trabajo sobre generación de autómatas. Ya se creó la clase Alfabeto.

---
+++ trunk/Codigo/automatas/src/lexico/Alfabeto.java
@@ -0,0 +1,77 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package lexico;
+
+import java.util.Arrays;
+import java.util.Vector;
+
+/**
+ * Clase que representa el alfabeto sobre el cual se construye
+ * una expresión regular.
+ * @author Germán Hüttemann
+ * @author Marcelo Rodas
+ */
+public class Alfabeto {
+    
+    /**
+     * Conjunto de símbolos del alfabeto.
+     */
+    private Vector<String> simbolos;
+    
+    /**
+     * Contructor de la clase.
+     * @param caracteres Cadena de caracteres con los símbolos del alfabeto.
+     */
+    public Alfabeto(String caracteres) {      
+        String[] arregloTemp = new String[caracteres.length()];
+        for (int i=0; i < caracteres.length(); i++)
+            arregloTemp[i] = "" + caracteres.charAt(i);
+        
+        Arrays.sort(arregloTemp);
+        
+        simbolos = new Vector<String>(arregloTemp.length);
+        for (int i=0; i < arregloTemp.length; i++) {
+            String temp = arregloTemp[i];
+            if (!simbolos.contains(temp))
+                simbolos.add(temp);
+        }   
+    }
+    
+    /**
+     * Retorna el tamaño de este alfabeto, es decir
+     * la cantidad de símbolos que contiene.
+     * @return Cantidad de símbolos del alfabeto.
+     */
+    public int getTamaño() {
+        return simbolos.size();
+    }
+    
+    /**
+     * Retorna un determinado símbolo del alfabeto.
+     * @param pos Posición del símbolo dentro del alfabeto.
+     * @return El símbolo del alfabeto ubicado en la posición <code>pos</code>.
+     */
+    public String getSimbolo(int pos) {
+        return simbolos.get(pos);
+    }
+    
+    /**
+     * Método que permite conocer si un caracter dado pertenece
+     * a este alfabeto.
+     * @param caracter El caracter que se quiere saber si pertenece al alfabeto.
+     * @return <code>true</code> si el caracter pertenece al alfabeto, <code>false</code> en caso contrario.
+     */
+    public boolean contiene(String caracter) {
+        return simbolos.contains(caracter);
+    }
+    
+    @Override
+    public String toString() {
+        String salida = "";
+        
+        return salida;
+    }
+}
