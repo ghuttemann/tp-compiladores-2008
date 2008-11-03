1013a6bb7f8421f9aa16384ada852cc6a0b96a92
ghuttemann <ghuttemann@03c27c54-a44f-11dd-98aa-2b35b7097d33>
2008-11-03 00:42:04 +0000

    Clase que representa un conjunto de elementos. Utilizada para: - El conjunto de transiciones de un estado de un autómata finito. - El conjunto de estados de un autómata finito.

---
+++ trunk/Codigo/automatas/src/generacion/Conjunto.java
@@ -0,0 +1,65 @@
+/*
+ * Trabajo Práctico de Compiladores 2008.
+ * 10mo Semestre Ingeniería Infomática.
+ * Facultad Politécnica - UNA.
+ */
+package generacion;
+
+import java.util.Vector;
+
+/**
+ * Esta clase representa un conjunto genérico. La misma es utilizada
+ * para los conjuntos de estados y transiciones, correspondientes a
+ * un autómata finito y a un determinado estado destino de los estados
+ * de éste.
+ * @param <T> El tipo de dato almacenado en esta conjunto.
+ * @author Germán Hüttemann
+ * @author Marcelo Rodas
+ */
+public class Conjunto<T> {
+    
+    /**
+     * Conjunto de elementos.
+     */
+    private Vector<T> elementos;
+    
+    /**
+     * Constructor de la clase.
+     */
+    public Conjunto() {
+        elementos = new Vector<T>();
+    }
+    
+    /**
+     * Agrega un nuevo elemento al conjunto.
+     * @param elemento El nuevo elemento a agregar.
+     */
+    public void agregar(T elemento) {
+        elementos.add(elemento);
+    }
+    
+    /**
+     * Elimina un elemento del conjunto.
+     * @param elemento El elemento a ser eliminado.
+     */
+    public void eliminar(T elemento) {
+        elementos.remove(elemento);
+    }
+    
+    /**
+     * Retorna el i-ésimo elemento del conjunto.
+     * @param i Posición del elemento a retornar.
+     * @return El elemento buscado.
+     */
+    public T obtener(int i) {
+        return elementos.get(i);
+    }
+    
+    /**
+     * Retorna la cantidad de transiciones del conjunto.
+     * @return Cantidad de transiciones del conjunto.
+     */
+    public int cantidad() {
+        return elementos.size();
+    }
+}
