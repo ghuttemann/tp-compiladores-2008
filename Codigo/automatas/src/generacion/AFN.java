97513d79ee414c8fb6b6d6c0f49384d59c88deb7
ghuttemann <ghuttemann@03c27c54-a44f-11dd-98aa-2b35b7097d33>
2008-11-03 01:38:07 +0000

    Clase que representa un AFN.

---
+++ Codigo/automatas/src/generacion/AFN.java
@@ -0,0 +1,131 @@
+/*
+ * Trabajo Práctico de Compiladores 2008.
+ * 10mo Semestre Ingeniería Infomática.
+ * Facultad Politécnica - UNA.
+ */
+package generacion;
+
+import analisis.Alfabeto;
+
+/**
+ * Esta clase representa un Autómata Finito No-determinístico, el cual
+ * es construído a partir de una expresión regular, a través del algoritmo
+ * de Thompson.<br><br>
+ * Por definición, un AFN puede tener más de un estado final. Sin embargo,
+ * los AFNs construídos con el algoritmo de Thompson siempre tienen un solo
+ * estado final.
+ * @author Germán Hüttemann
+ * @author Marcelo Rodas
+ */
+public class AFN {
+
+    /**
+     * Conjunto de estados del AFN.
+     */
+    private Conjunto<Estado> estados;
+    
+    /**
+     * Estado inicial del AFN.
+     */
+    private Estado inicio;
+    
+    /**
+     * Estado final del AFN.
+     */
+    private Estado fin;
+    
+    /**
+     * Expresión regular para este AFN.
+     */
+    private String exprReg;
+    
+    /**
+     * Alfabeto para este AFN.
+     */
+    private Alfabeto alfabeto;
+    
+    /**
+     * Constructor por defecto.
+     */
+    public AFN() {
+        this(null, "");
+    }
+    
+    public AFN(Alfabeto alfabeto, String exprReg) {
+        estados = new Conjunto<Estado>();
+        setAlfabeto(alfabeto);
+        setExprReg(exprReg);
+    }
+
+    /**
+     * Obtiene el estado inicial del AFN.
+     * @return El estado inicial del AFN.
+     */
+    public Estado getInicio() {
+        return inicio;
+    }
+
+    /**
+     * Setea el estado inicial del AFN.
+     * @param inicio El nuevo estado inicial del AFN.
+     */
+    public void setInicio(Estado inicio) {
+        this.inicio = inicio;
+    }
+    
+    /**
+     * Obtiene el estado final del AFN.
+     * @return El estado final del AFN.
+     */
+    public Estado getFin() {
+        return fin;
+    }
+
+    /**
+     * Obtiene el alfabeto de este AFN.
+     * @return El alfabeto de este AFN.
+     */
+    public Alfabeto getAlfabeto() {
+        return alfabeto;
+    }
+
+    /**
+     * Setea el alfabeto de este AFN.
+     * @param alfabeto El nuevo alfabeto para este AFN.
+     */
+    public void setAlfabeto(Alfabeto alfabeto) {
+        this.alfabeto = alfabeto;
+    }
+
+    /**
+     * Obtiene la expresión regular para este AFN.
+     * @return La expresión regular para este AFN.
+     */
+    public String getExprReg() {
+        return exprReg;
+    }
+
+    /**
+     * Setea la expresión regular para este AFN.
+     * @param exprReg La nueva expresión regular para este AFN.
+     */
+    public void setExprReg(String exprReg) {
+        this.exprReg = exprReg;
+    }
+
+    /**
+     * Setea el estado final del AFN.
+     * @param fin El estado final del AFN.
+     */
+    public void setFin(Estado fin) {
+        this.fin = fin;
+    }
+    
+    /**
+     * Agrega un estado al AFN.
+     * @param estado Nuevo estado para el AFN.
+     */
+    public void agregarEstado(Estado estado) {
+        estados.agregar(estado);
+    }
+}
