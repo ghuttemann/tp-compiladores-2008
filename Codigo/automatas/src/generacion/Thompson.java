72fff1826691e632717ffe2885babb0b2925892f
ghuttemann <ghuttemann@03c27c54-a44f-11dd-98aa-2b35b7097d33>
2008-11-04 18:53:31 +0000

    - Se agrega más comentario a la clase. - Se cambian los comentario, de // a /* */. - Se cambian los ciclos FOR por ciclos FOREACH. Consecuentemente se cambia la manera de asignar identificador a los estados. - Se termina de implementar el método "concatenacion".

--- Codigo/automatas/src/generacion/Thompson.java
+++ Codigo/automatas/src/generacion/Thompson.java
@@ -9,7 +9,16 @@ import analisis.Alfabeto;
 
 /**
  * Esta clase implementa los algoritmos de Thompson para cada
- * uno de los operadores de una expresión regular.
+ * uno de los operadores de una expresión regular.<br><br>
+ * Cabe destacar que las operaciones implementadas en esta
+ * clase tiene de efectos secundarios (side-effects) sobre 
+ * los AFN recibidos como argumentos ya que en ciertos casos
+ * estos resultan alterados. Sin embargo, a efectos prácticos
+ * esto no es un problema, ya que en la traducción de una 
+ * expresión regular a AFN siempre será significativo solo el
+ * último AFN construído.<br><br>
+ * La solución que podría aplicarse para corregir este efecto
+ * es implementar una función de copia para los AFN.
  * @author Germán Hüttemann
  * @author Marcelo Rodas
  */
@@ -24,19 +33,19 @@ public class Thompson {
     public static AFN basico(String simbolo) {
         AFN afn = new AFN();
         
-        // Estados inicial y final
+        /* Estados inicial y final */
         Estado ini = new Estado(0);
         Estado fin = new Estado(1);
         
-        // Transición entre los estados inicial y final
+        /* Transición entre los estados inicial y final */
         Transicion tran = new Transicion(ini, fin, simbolo);
         ini.getTransiciones().agregar(tran);
         
-        // Agregamos los estados al AFN
+        /* Agregamos los estados al AFN */
         afn.agregarEstado(ini);
         afn.agregarEstado(fin);
         
-        // Seteamos los estados inicial y final
+        /* Seteamos los estados inicial y final */
         afn.setEstadoInicial(ini);
         afn.setEstadoFinal(fin);
         
@@ -50,33 +59,33 @@ public class Thompson {
      */
     public static AFN cerraduraKleene(AFN afn) {
         AFN afn_salida = new AFN();
+        int contadorEstados = 0;
                 
-        // Agregamos el estado inicial
-        Estado nuevoInicio = new Estado(0);
+        /* Agregamos el estado inicial */
+        Estado nuevoInicio = new Estado(contadorEstados++);
         afn_salida.agregarEstado(nuevoInicio);
         afn_salida.setEstadoInicial(nuevoInicio);
         
-        // Agregamos los demás estados
-        for (int i=0; i < afn.getEstados().cantidad(); i++) {
-            Estado tmp = afn.getEstados().obtener(i);
-            tmp.setIdentificador(i + 1);
+        /* Agregamos los demás estados */
+        for (Estado tmp : afn.getEstados()) {
+            tmp.setIdentificador(contadorEstados++);
             afn_salida.agregarEstado(tmp);
         }
         
-        // Agregamos el estado final
-        Estado nuevoFin = new Estado(afn.getEstados().cantidad() + 1);
+        /* Agregamos el estado final */
+        Estado nuevoFin = new Estado(contadorEstados);
         afn_salida.agregarEstado(nuevoFin);
         afn_salida.setEstadoFinal(nuevoFin);
         
-        // Estados inicial y final anteriores
+        /* Estados inicial y final anteriores */
         Estado anteriorInicio = afn.getEstadoInicial();
         Estado anteriorFin    = afn.getEstadoFinal();
         
-        // Agregamos transiciones adicionales desde el nuevo estado inicial
+        /* Agregamos transiciones adicionales desde el nuevo estado inicial */
         nuevoInicio.getTransiciones().agregar(new Transicion(nuevoInicio, anteriorInicio, Alfabeto.VACIO));
         nuevoInicio.getTransiciones().agregar(new Transicion(nuevoInicio, nuevoFin, Alfabeto.VACIO));
         
-        // Agregamos transiciones adicionales desde el anterior estado final
+        /* Agregamos transiciones adicionales desde el anterior estado final */
         anteriorFin.getTransiciones().agregar(new Transicion(anteriorFin, anteriorInicio, Alfabeto.VACIO));
         anteriorFin.getTransiciones().agregar(new Transicion(anteriorFin, nuevoFin, Alfabeto.VACIO));
         
@@ -107,16 +116,39 @@ public class Thompson {
     
     public static AFN concatenacion(AFN afn1, AFN afn2) {
         AFN afn_salida = new AFN();
+        int contadorEstados = afn1.getEstadoFinal().getIdentificador() + 1;
         
-        // Agregar los estados de afn1.
-        for (int i=0; i < afn1.getEstados().cantidad(); i++) {
-            Estado tmp = afn1.getEstados().obtener(i);
+        /* Agregar los estados de afn1 */
+        for (Estado tmp : afn1.getEstados())
+            afn_salida.agregarEstado(tmp);
+        
+        /* 
+         * Agregar los estados de afn2, excepto el primero. Debemos 
+         * incrementar los identificadores de dichos estados.
+         */
+        for (int i=1; i < afn2.getEstados().cantidad(); i++) {
+            Estado tmp = afn2.getEstados().obtener(i);
+            tmp.setIdentificador(contadorEstados++);
             afn_salida.agregarEstado(tmp);
         }
         
-        // Setear estados inicial y final.
+        /*
+         * Agregamos las transiciones del estado inicial de afn2
+         * al estado final de afn1.
+         */
+        for (Transicion trans : afn2.getEstadoInicial().getTransiciones()) {
+            Transicion nuevaTrans = new Transicion();
+            
+            nuevaTrans.setOrigen(afn1.getEstadoFinal());
+            nuevaTrans.setDestino(trans.getDestino());
+            nuevaTrans.setSimbolo(trans.getSimbolo());
+            
+            afn1.getEstadoFinal().getTransiciones().agregar(nuevaTrans);
+        }
+        
+        // Setear nuevos estados inicial y final.
         afn_salida.setEstadoInicial(afn1.getEstadoInicial());
-        afn_salida.setEstadoFinal(afn1.getEstadoFinal());       
+        afn_salida.setEstadoFinal(afn2.getEstadoFinal());
         
         return afn_salida;
     }
