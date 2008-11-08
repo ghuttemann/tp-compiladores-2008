81cfc628874bfdc4c53650b211789eee5898c087
ghuttemann <ghuttemann@03c27c54-a44f-11dd-98aa-2b35b7097d33>
2008-11-08 15:24:22 +0000

    Nueva clase que representa un estado de un Autómata.

---
+++ Codigo/automatas/src/generacion/Estado.java
@@ -0,0 +1,143 @@
+/*
+ * Trabajo Práctico de Compiladores 2008.
+ * 10mo Semestre Ingeniería Infomática.
+ * Facultad Politécnica - UNA.
+ */
+package generacion;
+
+/**
+ * Esta clase representa un estado para un Autómata Finito.
+ * @author Germán Hüttemann
+ * @author Marcelo Rodas
+ */
+public class Estado implements Comparable<Estado> {
+
+    /**
+     * Identificador de este Estado.
+     */
+    private Integer identificador;
+    
+    /**
+     * Variable que indica si el Estado es final.
+     */
+    private Boolean esFinal;
+    
+    /**
+     * Conjunto de transiciones del Estado.
+     */
+    private Conjunto<Transicion> transiciones;
+    
+    /**
+     * Crea un <code>Estado</code> no final sin identificador.
+     */
+    public Estado() {
+        this(null, false);
+    }
+    
+    /**
+     * Crea un <code>Estado</code> no final con un identificador determinado.
+     * @param identificador El identificador del nuevo estado.
+     */
+    public Estado(Integer identificador) {
+        this(identificador, false);
+    }
+    
+    /**
+     * Crea un <code>Estado</code> con un identificador determinado, 
+     * que será final o no de acuerdo al valor de <code>esFinal</code>.
+     * @param identificador El identificador para este <code>Estado</code>.
+     * @param esFinal Determina si el <code>Estado</code> es final o no.
+     */
+    public Estado(Integer identificador, Boolean esFinal) {
+        setIdentificador(identificador);
+        setEsFinal(esFinal);
+        transiciones = new Conjunto<Transicion>();
+    }
+    
+    /**
+     * Establece un nuevo identificador para este <code>Estado</code>.
+     * @param identificador El nuevo identificador para este <code>Estado</code>.
+     */
+    public void setIdentificador(Integer identificador) {
+        this.identificador = identificador;
+    }
+    
+    /**
+     * Obtiene el identificador para este <code>Estado</code>.
+     * @return El identificador del <code>Estado</code>.
+     */
+    public Integer getIdentificador() {
+        return identificador;
+    }
+    
+    /**
+     * Obtiene el estado de aceptación del <code>Estado</code>.
+     * @return <code>true</code> si el <code>Estado</code> es final, 
+     * <code>false</code> en caso contrario.
+     */
+    public Boolean getEsFinal() {
+        return esFinal;
+    }
+
+    /**
+     * Establece un nuevo estado de aceptación para este <code>Estado</code>.
+     * @param esFinal El nuevo estado de aceptación para este <code>Estado</code>.
+     * Si el valor <code>true</code> el <code>Estado</code> se convertirá en
+     * final, si es <code>false</code> el <code>Estado</code> se convertirá
+     * en no final.
+     */
+    public void setEsFinal(Boolean esFinal) {
+        this.esFinal = esFinal;
+    }
+    
+    /**
+     * Obtiene el conjunto de transiciones de este <code>Estado</code>.
+     * @return El conjunto de transiciones del <code>Estado</code>.
+     */
+    public Conjunto<Transicion> getTransiciones() {
+        return transiciones;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        
+        final Estado other = (Estado) obj;
+        if (this.identificador.equals(other.identificador)) {
+            return false;
+        }
+        
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 41 * hash + this.identificador;
+        return hash;
+    }
+
+    public int compareTo(Estado obj) {
+        return this.identificador - obj.identificador;
+    }
+    
+    @Override
+    public String toString() {
+        String valor = String.valueOf(identificador);
+        
+        if (getIdentificador() == 0)
+            valor += "(i)";
+        else if (getEsFinal())
+            valor += "(f)";
+        else
+            valor += "   ";
+        
+        return valor;
+    }
+}
