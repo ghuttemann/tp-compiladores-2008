1ccd0f8296aae29e26c970b98333d9a0251d87a1
ghuttemann <ghuttemann@03c27c54-a44f-11dd-98aa-2b35b7097d33>
2008-11-08 20:46:37 +0000

    Se mudan estas clases a la parte de test.

---
+++ trunk/Codigo/automatas/test/analisis/PruebaAnalisisSintactico.java
@@ -0,0 +1,34 @@
+/*
+ * Trabajo Práctico de Compiladores 2008.
+ * 10mo Semestre Ingeniería Infomática.
+ * Facultad Politécnica - UNA.
+ */
+package analisis;
+
+import generacion.Automata;
+
+/**
+ *
+ * @author Germán Hüttemann
+ * @author Marcelo Rodas
+ */
+public class PruebaAnalisisSintactico {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Alfabeto alfa = new Alfabeto("ab");
+        String er = "(a|b)*a";
+        AnalizadorSintactico as = new AnalizadorSintactico(alfa, er);
+        
+        Automata salida;        
+        try {
+            salida = as.analizar();
+            System.out.printf("%s", salida);
+        }
+        catch (Exception ex) {
+            System.out.println(ex);
+        }
+    }
+}
