30a775dbdcb83103f06811c94d0b0c82ae26c425
ghuttemann <ghuttemann@03c27c54-a44f-11dd-98aa-2b35b7097d33>
2008-11-10 16:39:58 +0000

    Clase para AFD. Hereda de Automata y agrega el conjunto "estadosD".

---
+++ trunk/Codigo/automatas/src/generacion/AFD.java
@@ -0,0 +1,68 @@
+/*
+ * Trabajo Práctico de Compiladores 2008.
+ * 10mo Semestre Ingeniería Infomática.
+ * Facultad Politécnica - UNA.
+ */
+package generacion;
+
+import analisis.Alfabeto;
+
+/**
+ * Clase que representa la abstracción para un Autómata Finito,
+ * Determinístico (AFD). Un AFD es generado a partir de un AFN 
+ * a través del algoritmo de Subconjuntos.Adicionalmente, un AFD 
+ * puede ser reducido (en cuanto a cantidad de estados se refiere)
+ * a través del algoritmo de Minimización de Estados.
+ * @author Germán Hüttemann
+ * @author Marcelo Rodas
+ */
+public class AFD extends Automata {
+    
+    /**
+     * Conjunto de estados del AFN contenidos
+     * en cada uno de los estados de este AFD.
+     */
+    private Conjunto<Conjunto<Estado>> estadosD;
+    
+    /**
+     * Constructor por defecto.
+     */
+    public AFD() {
+       this(null, "");
+    }
+    
+    /**
+     * Construye un <code>AFD</code> con un determinado <code>Alfabeto</code>
+     * y una determinada expresión regular.
+     * @param alfabeto El <code>Alfabeto</code> de este <code>AFD</code>.
+     * @param exprReg La expresión regular para este <code>AFD</code>.
+     */
+    public AFD(Alfabeto alfabeto, String exprReg) {
+        super(alfabeto, exprReg);
+        estadosD = new Conjunto<Conjunto<Estado>>();
+    }
+    
+    /**
+     * Obtiene el <code>Conjunto</code> de <code>Estado</code>s 
+     * del <code>AFN</code> contenidos en cada uno de los 
+     * <code>Estado</code>s de este <code>AFD</code>.
+     * @return El <code>Conjunto</code> de <code>Estado</code>s 
+     * contenidos en cada uno de los <code>Estado</code>s de 
+     * este <code>AFD</code>.
+     */
+    public Conjunto<Conjunto<Estado>> getEstadosD() {
+        return estadosD;
+    }
+
+    /**
+     * Establece el <code>Conjunto</code> de <code>Estado</code>s 
+     * del <code>AFN</code> contenidos en cada uno de los 
+     * <code>Estado</code>s de este <code>AFD</code>.
+     * @param estadosD El nuevo <code>Conjunto</code> de 
+     * <code>Estado</code>s contenidos en cada uno de los 
+     * <code>Estado</code>s de este <code>AFD</code>.
+     */
+    public void setEstadosD(Conjunto<Conjunto<Estado>> estadosD) {
+        this.estadosD = estadosD;
+    }
+}
