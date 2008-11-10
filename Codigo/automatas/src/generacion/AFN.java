52210b546fbda042400b807dd15aed3e6964763f
ghuttemann <ghuttemann@03c27c54-a44f-11dd-98aa-2b35b7097d33>
2008-11-10 16:33:46 +0000

    Clase para AFN. Hereda de Automata y, actualmente, no agrega nada.

---
+++ Codigo/automatas/src/generacion/AFN.java
@@ -0,0 +1,35 @@
+/*
+ * Trabajo Práctico de Compiladores 2008.
+ * 10mo Semestre Ingeniería Infomática.
+ * Facultad Politécnica - UNA.
+ */
+package generacion;
+
+import analisis.Alfabeto;
+
+/**
+ * Clase que representa la abstracción para un Autómata Finito,
+ * No determinístico (AFN). Un AFN es contruído a partir de una 
+ * expresión regular a través de las construcciones de Thompson.
+ * @author Germán Hüttemann
+ * @author Marcelo Rodas
+ */
+public class AFN extends Automata {
+        
+    /**
+     * Constructor por defecto.
+     */
+    public AFN() {
+       super();
+    }
+    
+    /**
+     * Construye un <code>AFN</code> con un determinado <code>Alfabeto</code>
+     * y una determinada expresión regular.
+     * @param alfabeto El <code>Alfabeto</code> de este <code>AFN</code>.
+     * @param exprReg La expresión regular para este <code>AFN</code>.
+     */
+    public AFN(Alfabeto alfabeto, String exprReg) {
+        super(alfabeto, exprReg);
+    }
+}
