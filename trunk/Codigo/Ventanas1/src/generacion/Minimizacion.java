83e0ed9aa191bfc921783a5a2edabf322c1f453f
ghuttemann <ghuttemann@03c27c54-a44f-11dd-98aa-2b35b7097d33>
2008-11-12 04:51:49 +0000

    Se agregan los métodos eliminarInalcanzables y recuperarAlcanzados.

--- trunk/Codigo/Ventanas1/src/generacion/Minimizacion.java
+++ trunk/Codigo/Ventanas1/src/generacion/Minimizacion.java
@@ -5,6 +5,8 @@
  */
 package generacion;
 
+import java.util.Stack;
+
 /**
  * Esta clase implementa el algoritmo de minimización de 
  * estados de un AFD.
@@ -45,7 +47,46 @@ public class Minimizacion {
     }
     
     private static void eliminarInalcanzables(AFD afd) {
+        /* Conjunto de estados alcanzados desde el estado inicial */
+        Conjunto<Estado> alcanzados = recuperarAlcanzados(afd);
         
+        /* Eliminamos los estados no alcanzados */
+        afd.getEstados().retener(alcanzados);
+        
+        /* Actualizamos los identificadores de los estados */
+        // TODO
+    }
+    
+    private static Conjunto<Estado> recuperarAlcanzados(AFD afd) {
+        /* Estado inicial del recorrido */
+        Estado actual = afd.getEstadoInicial();
+        
+        /* Conjunto de estados alcanzados */
+        Conjunto<Estado> alcanzados = new Conjunto<Estado>();
+        
+        /* Agregamos el estad actual */
+        alcanzados.agregar(actual);
+        
+        /* Pila para almacenar los estados pendientes */
+        Stack<Estado> pila = new Stack();
+        
+        /* Meter el estado actual como el estado inicial */
+        pila.push(actual);
+        
+        while (!pila.isEmpty()) {
+            actual = pila.pop();
+            
+            for (Transicion t : actual.getTransiciones()) {
+                Estado e = t.getEstado();
+                
+                if (!alcanzados.contiene(e)) {
+                    alcanzados.agregar(e);
+                    pila.push(e);
+                }
+            }
+        }
+        
+        return alcanzados;
     }
     
     private static void eliminarIdentidades(AFD afd) {
