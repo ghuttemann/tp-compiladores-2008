0f79ab2dde0d3d50d623ead158f2eb5150486c12
ghuttemann <ghuttemann@03c27c54-a44f-11dd-98aa-2b35b7097d33>
2008-11-17 02:48:31 +0000

    Clase que implementa los algoritmos de validación de AFDs y AFNs.

---
+++ trunk/Codigo/Ventanas1/src/generacion/Validacion.java
@@ -0,0 +1,23 @@
+/*
+ * Trabajo Práctico de Compiladores 2008.
+ * 10mo Semestre Ingeniería Infomática.
+ * Facultad Politécnica - UNA.
+ */
+package generacion;
+
+/**
+ * Esta clase implementa los algoritmos de validación
+ * para AFNs y AFDs.
+ * @author Germán Hüttemann
+ * @author Marcelo Rodas
+ */
+public class Validacion {
+
+    public static void validarAFN(AFN afn, String entrada) {
+        
+    }
+
+    public static void validarAFD(AFD afd, String entrada) {
+        
+    }
+}
