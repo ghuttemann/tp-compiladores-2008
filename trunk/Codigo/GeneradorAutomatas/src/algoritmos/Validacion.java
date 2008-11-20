8c6621fdad05892e4e9187f2430d616715765993
ghuttemann <ghuttemann@03c27c54-a44f-11dd-98aa-2b35b7097d33>
2008-11-20 03:13:14 +0000

    - Se implementa la validación de AFN. - Se utiliza la clase Par para guardar, además del camino de estados, también las transiciones.

--- trunk/Codigo/GeneradorAutomatas/src/algoritmos/Validacion.java
+++ trunk/Codigo/GeneradorAutomatas/src/algoritmos/Validacion.java
@@ -27,7 +27,75 @@ public class Validacion {
      * los resultados de la validación.
      */
     public static ResultadoValidacion validarAFN(AFN afn, String entrada) {
-        return null;
+        /* Buffer para manipular la entrada */
+        Queue<String> buffer = new LinkedList<String>();
+        
+        /*
+         * Agregamos los caracteres de la entrada en
+         * forma FIFO. Cuando se consuman todos los
+         * caracteres, el siguiente será null, que
+         * representará al caracter EOF.
+         */
+        for (int i=0; i < entrada.length(); i++)
+            buffer.add("" + entrada.charAt(i));
+        
+        /* Secuencia de conjuntos de estados recorridos */
+        Conjunto<Par<Conjunto<Estado>, String>> camino = new Conjunto<Par<Conjunto<Estado>, String>>();
+        
+        /* Comenzamos por la cerradura del estado inicial del AFN */
+        Conjunto<Estado> estadosActuales = Subconjuntos.cerraduraEpsilon(afn.getEstadoInicial());
+        camino.agregar(new Par<Conjunto<Estado>, String>(estadosActuales));
+        
+        /* Cada simbolo de entrada */
+        String simbolo;
+        
+        /* Recorremos mientras hayan simbolos en la entrada */
+        while ((simbolo = buffer.poll()) != null) {
+            /* Nos movemos al siguiente estado */
+            estadosActuales = Subconjuntos.mover(estadosActuales, simbolo);
+            
+            if (estadosActuales.estaVacio()) {
+                /* Si no se alcanza ningún estado, terminamos */
+                break;
+            }
+            else {
+                /* Agregar el estado nuevo al camino */
+                camino.agregar(new Par<Conjunto<Estado>, String>(estadosActuales, simbolo));
+            }
+        }
+        
+        /*
+         * Debemos comprobar cuál fue la condición que
+         * provocó la finalización del ciclo while y 
+         * reaccionar de acuerdo a ello.
+         */
+        if (estadosActuales.estaVacio()) {
+            /*
+             * Llegamos a un estado desde el cual
+             * no se pudo avanzar con el símbolo
+             * actual.
+             * Recuperamos todos los simbolos no
+             * consumidos.
+             */
+            String entradaFaltante = simbolo;
+            for (String s : buffer)
+                entradaFaltante += s;
+            
+            /*
+             * Retornamos el camino construido y la
+             * subcadena que faltó consumir de la
+             * entrada.
+             */
+            return new ResultadoValidacionAFN(afn, entrada, camino, entradaFaltante);
+        }
+        else {
+            /* 
+             * Hemos consumido toda la entrada, por lo
+             * que retornamos el camino construido y
+             * la cadena vacía como entrada faltante.
+             */
+            return new ResultadoValidacionAFN(afn, entrada, camino, "");
+        }
     }
 
     /**
@@ -53,11 +121,11 @@ public class Validacion {
             buffer.add("" + entrada.charAt(i));
         
         /* Secuencia de estados recorridos */
-        Conjunto<Estado> camino = new Conjunto<Estado>();
+        Conjunto<Par<Estado, String>> camino = new Conjunto<Par<Estado, String>>();
         
         /* Comenzamos por el estado inicial del AFD */
         Estado estadoActual = afd.getEstadoInicial();
-        camino.agregar(estadoActual);
+        camino.agregar(new Par<Estado, String>(estadoActual));
         
         /* Cada simbolo de entrada */
         String simbolo;
@@ -73,7 +141,7 @@ public class Validacion {
             }
             else {
                 /* Agregar el estado nuevo al camino */
-                camino.agregar(estadoActual);
+                camino.agregar(new Par<Estado, String>(estadoActual, simbolo));
             }
         }
         
@@ -95,11 +163,11 @@ public class Validacion {
                 entradaFaltante += s;
             
             /*
-             * Retornamos el camino construidos y la
+             * Retornamos el camino construido y la
              * subcadena que faltó consumir de la
              * entrada.
              */
-            return new ResultadoValidacion(afd, entrada, camino, entradaFaltante);
+            return new ResultadoValidacionAFD(afd, entrada, camino, entradaFaltante);
         }
         else {
             /* 
@@ -107,7 +175,7 @@ public class Validacion {
              * que retornamos el camino construido y
              * la cadena vacía como entrada faltante.
              */
-            return new ResultadoValidacion(afd, entrada, camino, "");
+            return new ResultadoValidacionAFD(afd, entrada, camino, "");
         }
     }
 
